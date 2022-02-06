6a2090ed4a9e1c87d25d0a8bfd5d5cc5f5ad63a9
seungahkim <safg0421@cau.ac.kr>
2022-02-06 21:32:25 +0900

    feat: create Roome considering limit
    1. 어드민에서  Room을 중보복 생성 시 경고 message
    2. 깨Room의 클래스 변수
       - CAPACITY: 기본 ì인원ì 
       - EXTRA_ROOM: ã남ë인원 수가 더 많은 바ìë방 갯수
    3. is_extra_room_left
    
    ãsettings
    1. logger 추가
    2. ëAUTH_USER_MODEL 주석 추가
    
    chore
    mento -> mentor

--- PrettyLion/playground/admin.py
+++ PrettyLion/playground/admin.py
@@ -1,3 +1,27 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
-# Register your models here.
+from .models import Room
+
+import logging
+logger = logging.getLogger('django.server')
+
+
+class RoomAdmin(admin.ModelAdmin):
+    readonly_fields = ('mentor', 'limit')
+
+    def save_model(self, request, obj, form, change):
+        # update
+        if obj.id:
+            super().save_model(request, obj, form, change)
+        # create
+        else:
+            room = Room.objects.filter(mentor=request.user).exists()
+            if room:
+                messages.add_message(request, messages.ERROR, "이미 만든 방이 있어요. >_^")
+            else:
+                logger.info(f'[New Room Info] name: {obj.name} limit: {obj.limit}')
+                obj.mentor = request.user
+                super().save_model(request, obj, form, change)
+
+
+admin.site.register(Room, RoomAdmin)
--- PrettyLion/playground/models.py
+++ PrettyLion/playground/models.py
@@ -1,7 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+from .constants import NUMBER_OF_MENTEE, NUMBER_OF_MENTO
+
+
+def set_limit():
+    if Room.is_extra_room_left():
+        return Room.CAPACITY + 1
+    else:
+        return Room.CAPACITY
+
 
 class Room(models.Model):
+    CAPACITY = NUMBER_OF_MENTEE // NUMBER_OF_MENTO
+    EXTRA_ROOM = NUMBER_OF_MENTEE % NUMBER_OF_MENTO
+
     name = models.CharField(max_length=64)
-    limit = models.PositiveSmallIntegerField(blank=True)
+    limit = models.PositiveSmallIntegerField(default=set_limit, blank=True)
     chat_url = models.URLField()
-#    mento = models.OneToOneField(AUTH_USER_MODEL)
+    mentor = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, blank=True)
+
+    def delete(self, using=None, keep_parents=False):
+        super().delete(using, keep_parents)
+
+    @staticmethod
+    def is_extra_room_left():
+        count = Room.objects.filter(limit=Room.CAPACITY + 1).count()
+        return True if Room.EXTRA_ROOM > count else False
+
+    def __str__(self):
+        return f"{self.name} ({self.mentor if hasattr(self, 'mentor') else 'None'})"
