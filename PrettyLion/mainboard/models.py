2b8683c4ba2308f573a1dfe80c1221df846bb203
myeongjunkim <ilikerunning@naver.com>
2022-02-07 02:51:40 +0900

    feat: aboutus model

--- PrettyLion/mainboard/models.py
+++ PrettyLion/mainboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class AboutUs(models.Model):
+    image = models.ImageField(null=True, upload_to="aboutus/", blank=True)
+    name = models.CharField(max_length=20)
+    aboutu = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
