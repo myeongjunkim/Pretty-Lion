3cd91c048aed49e3d89222336aa71b85af328078
myeongjunkim <ilikerunning@naver.com>
2022-02-07 02:46:33 +0900

    feat: aboutus front

--- PrettyLion/mainboard/views.py
+++ PrettyLion/mainboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def view_index(request):
     return render(request, 'index.html')
+
+def view_aboutus(request):
+    return render(request, 'aboutus.html')
