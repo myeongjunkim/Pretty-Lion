21a678f58b2c950bce4830c78b9f8564bf9f0d66
unanchoi <unusualc@likelion.org>
2022-02-14 03:35:37 +0900

    feat : add signup model, signup view

--- PrettyLion/accounts/models.py
+++ PrettyLion/accounts/models.py
@@ -2,14 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
+
 class User(AbstractUser):
     nickname = models.CharField(max_length=20)
-    grade = models.PositiveSmallIntegerField
-    age: models.PositiveSmallIntegerField
-    profile_photo = models.ImageField(blank=False, null=True, upload_to="accounts_photo/")
-    bio = models.CharField(max_length=30)
-    realname = models.CharField(max_length=15)
-    major = models.CharField(max_length=15)
+    grade = models.SmallIntegerField(default=0)
+    age: models.SmallIntegerField(default=0)
+    profile_photo = models.ImageField(blank=True, null=True, upload_to="accounts_photo/")
+    bio = models.CharField(max_length=30, blank=True)
+    realname = models.CharField(max_length=15, blank=True)
+    major = models.CharField(max_length=15, blank=True)
+
 
 
 # Create your models here.
--- PrettyLion/accounts/views.py
+++ PrettyLion/accounts/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render,redirect
 from django.contrib import auth
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST' or request.method == "FILES":
+            if request.POST['password'] == request.POST['password_check']:
+                if request.POST.get("mento_selection") == "mento":
+                    is_mento = True
+                else:
+                    is_mento = False
+
+                user = User.objects.create_user(
+                username =request.POST["username"],
+                password =request.POST['password'],
+                nickname =request.POST['nickname'],
+                bio=request.POST['bio'])
+                profile_photo =request.FILES['profile_photo'],
+                is_staff  =is_mento,
+
+                auth.login(request, user)
+                return redirect('index')
+            else:
+                return render(request, "signup.html", {'password_error'
+                                                       : "비밀번호를 잘 못 입력하셨습니다!"})
+    else:
+        return render(request, 'signup.html')
 
 def login(request):
     if request.method == "POST":
