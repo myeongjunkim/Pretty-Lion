7ed7de9e90a51dc94586691e52c7612e5d4031d3
unanchoi <unusualc@likelion.org>
2022-02-14 01:49:33 +0900

    feat : add view -  login, logout

--- PrettyLion/accounts/views.py
+++ PrettyLion/accounts/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
     return render(request, 'signup.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        userid = request.POST['username']
+        pw = request.POST['password']
+        user = auth.authenticate(request, username=userid, password=pw)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+
 
 def logout(request):
-    return render(request, 'logout.html')
+    auth.logout(request)
+    return redirect(request, 'index')
 
