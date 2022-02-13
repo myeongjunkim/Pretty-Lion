5e1c68620ca866221facc8819c4b6e2f5bf3a52e
unanchoi <unusualc@likelion.org>
2022-02-14 01:23:45 +0900

    feat : add signup.css initial setting

--- PrettyLion/accounts/urls.py
+++ PrettyLion/accounts/urls.py
@@ -5,7 +5,7 @@ from  django.conf.urls.static import static
 
 urlpatterns = [
     path('login/', views.login, name="login"),
-    path("signup/", views.signup, name = "signup"),
-    path("logout", views.logout, name = "logout"),
+    path("signup/", views.signup, name="signup"),
+    path("logout", views.logout, name="logout"),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
