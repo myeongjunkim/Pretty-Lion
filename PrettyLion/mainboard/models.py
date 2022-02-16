64b67668d4bddb499a98f923dff7dc44a407847c
myeongjunkim <ilikerunning@naver.com>
2022-02-16 21:20:57 +0900

    feat: aboutus create

--- PrettyLion/mainboard/models.py
+++ PrettyLion/mainboard/models.py
@@ -7,7 +7,6 @@ class AboutUs(models.Model):
     image = models.ImageField(null=True, upload_to="aboutus/", blank=True)
     name = models.CharField(max_length=20)
     aboutu = models.TextField()
-    # have to add user foreignkey
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
 
--- PrettyLion/mainboard/views.py
+++ PrettyLion/mainboard/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import AboutUs
 
 # Create your views here.
 def view_index(request):
     return render(request, 'index.html')
 
 def view_aboutus(request):
-    return render(request, 'aboutus.html')
+    aboutus_data = AboutUs.objects.all()
+    return render(request, 'aboutus.html', {"aboutus_data":aboutus_data})
 
 def create_aboutus(request):
-    pass
+    if request.method == "POST":
+        new_aboutus = AboutUs()
+        new_aboutus.image = request.FILES['aboutus_image']
+        new_aboutus.name = request.POST['aboutus_name']
+        new_aboutus.aboutu = request.POST['aboutus_aboutu']
+        new_aboutus.user = request.user
+        new_aboutus.save()
+        return redirect('aboutus')
+
+def delete_aboutus(request, id):
+    delete_aboutus = AboutUs().objects.get(id= id)
+    delete_aboutus.delete()
+    return redirect('aboutus')
+
+    
