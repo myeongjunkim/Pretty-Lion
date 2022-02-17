194f521684304c1a43124de3cbaec7a00216a8d7
unanchoi <unusualc@likelion.org>
2022-02-18 02:34:01 +0900

    fix: nav-url, logout view

--- PrettyLion/PrettyLion/urls.py
+++ PrettyLion/PrettyLion/urls.py
@@ -16,13 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainboard.urls')),
+
     path("accounts/", include("accounts.urls")),
     path('playground/', include('playground.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+]
--- PrettyLion/accounts/urls.py
+++ PrettyLion/accounts/urls.py
@@ -6,6 +6,6 @@ from  django.conf.urls.static import static
 urlpatterns = [
     path('login/', views.login, name="login"),
     path("signup/", views.signup, name="signup"),
-    path("logout", views.logout, name="logout"),
+    path("logout/", views.logout, name="logout"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
