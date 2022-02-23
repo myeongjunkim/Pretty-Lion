e775075f8966f17037640a0d08c63987a3af9120
myeongjunkim <ilikerunning@naver.com>
2022-02-23 15:56:33 +0900

    feat:plg_choice page

--- PrettyLion/playground/urls.py
+++ PrettyLion/playground/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', view_plg_intro, name="plg-intro"),
     path('info/', view_plg_info, name="plg-info"),
     path('qna/', view_plg_qna, name="plg-qna"),
+    path('qna-last/', view_plg_qna_last, name="plg-qna-last"),
+    path('choice/', view_plg_choice, name="plg-choice"),
     path('mentor-rooms/<int:pk>/', MentorRoomDetailView.as_view(), name='mentor-room-detail'),
     path('mentor-rooms/match/', MentorRoomMatchView.as_view(), name='mentor-room-match'),
 ]
