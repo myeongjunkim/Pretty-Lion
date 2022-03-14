569c87fc4db3b8ddce2803adbcf2e1038790cade
seungahkim <safg0421@cau.ac.kr>
2022-03-15 04:02:06 +0900

    feat: add context matching answer id & slice 4 answer

--- PrettyLion/playground/views.py
+++ PrettyLion/playground/views.py
@@ -56,16 +56,22 @@ class MentorRoomMatchView(LoginRequiredMixin, ListView):
         # join mentor room, mentor
         mentor_room_list = MentorRoom.objects.all().prefetch_related('mentor', 'mentor__answer_set')
         context = super(MentorRoomMatchView, self).get_context_data(object_list=mentor_room_list, ** kwargs)
-        context['matching_temperature'] = self.calculate_matching_score(mentor_room_list)
+        # get match data
+        matching_answer, matching_temperature = self.calculate_matching_score(mentor_room_list)
+        context['matching_answer'] = matching_answer
+        context['matching_temperature'] = matching_temperature
         return context
 
     def calculate_matching_score(self, mentor_room_list):
         user_answers = set(self.request.user.answer_set.values_list('choice', flat=True))
-        matching_score = {}
+        matching_answer = {}
+        matching_temperature = {}
         for mentor_room in mentor_room_list:
-            score = len(set(user_answers).intersection(mentor_room.mentor.answer_set.values_list('choice', flat=True)))
-            matching_score[mentor_room.id] = self.convert_temperature(score)
-        return matching_score
+            answer = set(user_answers).intersection(mentor_room.mentor.answer_set.values_list('choice', flat=True))
+            score = len(answer)
+            matching_answer[mentor_room.id] = list(answer)
+            matching_temperature[mentor_room.id] = self.convert_temperature(score)
+        return matching_answer, matching_temperature
 
     def convert_temperature(self, score):
         return 36 + (score * self.WEIGHT)
