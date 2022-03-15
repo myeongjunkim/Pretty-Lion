90575e0d589ea2409761bbf4da6f54350bab8f1f
seungahkim <safg0421@cau.ac.kr>
2022-03-16 01:58:59 +0900

    feat: catch Unique error and add message&redirect

--- PrettyLion/playground/views.py
+++ PrettyLion/playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import ModelFormMixin, ProcessFormView
 from django.http import HttpResponseRedirect
 from django.db.models import Count, F
+from django.db import IntegrityError
+
 
 from .models import MentorRoom, Question, Answer, Mentee
 
@@ -49,7 +52,11 @@ def mentoring(request):
     """
     mentor_room_id = request.POST["mentor_room"]
     mentor_room = get_object_or_404(MentorRoom, id=mentor_room_id)
-    Mentee.objects.create(user=request.user, mentor_room=mentor_room)
+    try:
+        Mentee.objects.create(user=request.user, mentor_room=mentor_room)
+    except IntegrityError:
+        messages.add_message(request, messages.ERROR, '이미 참여한 멘토방이 존재합니다.')
+        return redirect('mentor-room-match')
     return HttpResponseRedirect(reverse('mentor-room-detail', kwargs={"pk": mentor_room_id}))
 
 
